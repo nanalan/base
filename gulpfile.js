189db5465e936b995a33f97825dfa6be702f38dc
Alex <16batesa@gmail.com>
2016-10-08 21:51:38 +0100

    1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,129 @@
+const gulp = require('gulp')
+const util = require('gulp-util')
+const fs = require('fs')
+
+gulp.task('build:css', resolve => {
+  const stylus = require('stylus') // gulp-stylus doesn't work w/ gulp 4
+  const postcss = require('postcss')
+
+  function reject(err) {
+    throw new util.PluginError({
+      plugin: 'build:css',
+      message: err.message
+    })
+  }
+
+  fs.readFile('src/style.styl', 'utf8', (err, style) => {
+    if(err) {
+      throw new util.PluginError({
+        plugin: 'build:css',
+        message: err.message
+      })
+    } else {
+      stylus(style)
+        .set('filename', 'src/style.styl')
+        .set('sourcemap', { inline: true })
+        .set('paths', [__dirname + '/src'])
+        .render((err, css) => {
+          if(err) {
+            reject(err)
+          } else {
+            postcss([
+              require('postcss-import')({ plugins: [
+                require('cssnano')({ safe: true })
+              ] }),
+              require('rucksack-css')({ fallbacks: true, autoprefixer: true }),
+              require('cssnano')({ safe: true }),
+            ])
+              .process(css, {
+                from: 'src/style.styl',
+                to: 'dist/style.css',
+                map: {
+                  inline: false,
+                },
+              })
+              .catch(reject)
+              .then(res => {
+                fs.writeFile('dist/style.css', res.css, 'utf8', err => {
+                  if(err) reject(err)
+                  else {
+                    fs.writeFile('dist/style.css.map', res.map, 'utf8', err => {
+                      if(err) reject(err)
+                      else resolve()
+                    })
+                  }
+                })
+              })
+          }
+        })
+    }
+  })
+})
+
+gulp.task('watch:css', () => gulp.watch('src/**/*.styl', gulp.series('build:css')))
+
+function buildJs(resolve) {
+  const browserify = require('browserify')
+  const watchify = require('uber-watchify')
+
+  const sourcemaps = require('gulp-sourcemaps')
+
+  const transform = require('vinyl-transform')
+  const source = require('vinyl-source-stream')
+  const buffer = require('vinyl-buffer')
+
+  function reject(err) {
+    throw new util.PluginError({
+      plugin: 'build:js',
+      message: err.message
+    })
+  }
+
+  let opts = Object.assign({}, watchify.args, {
+    entries: ['src/script.js'],
+    debug: true,
+    watch: true,
+  })
+
+  let b = watchify(browserify(opts))
+
+  b.transform(require('babelify').configure({
+    presets: 'latest',
+    plugins: 'transform-runtime',
+  }))
+
+  b.on('update', () => {
+    util.log(`Starting '${util.colors.cyan(`build:js`)}'...`)
+    build()
+  })
+
+  b.on('log', what => util.log(`Finished '${util.colors.cyan(`build:js`)}': ` + what))
+
+  function build() {
+    return b.bundle()
+      .on('error', reject)
+      .on('end', resolve)
+      .pipe(source('script.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(require('gulp-uglify')())
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('dist'))
+  }
+
+  return build
+}
+
+gulp.task('build:js', resolve => {
+  return buildJs(() => {
+    resolve()
+    process.exit()
+  })()
+})
+
+gulp.task('watch:js', () => {
+  buildJs(() => {})()
+})
+
+gulp.task('build', gulp.parallel('build:js', 'build:css'))
+gulp.task('watch', gulp.parallel('watch:js', 'watch:css'))
---
+++ readme.md
@@ -0,0 +1,25 @@
+# base
+> my very own gulpfile
+
+`src/style.styl` > [Stylus](http://stylus-lang.com/) + [Rucksack](https://simplaio.github.io/rucksack/) + [Normalize.css](https://necolas.github.io/normalize.css/) [cssnano](http://cssnano.co/) > `dist/style.css`
+
+`src/script.js` > [Babel (ES2017)](https://babeljs.io/) + [Browserify](http://browserify.org/) [Uglify2](http://lisperator.net/uglifyjs/) > `dist/script.js`
+
+### ready
+```sh
+$ npm install -g gulp-cli
+```
+
+### set
+```sh
+$ gulp --tasks
+```
+
+### go
+```sh
+$ gulp
+```
+
+### _base_
+1. _A foundation or starting point for further work._
+2. _A main or important element or ingredient to which other things are added.
